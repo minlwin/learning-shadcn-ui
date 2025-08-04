ba6b43dbc18528a9e7054bedc35d99b37070aa5b
Min Lwin <lwin.zawmin@gmail.com>
2025-08-05 02:10:36 +0630

    For Multiple Select

--- using-forms/app/routes/basic/select/select-many.tsx
+++ using-forms/app/routes/basic/select/select-many.tsx
@@ -1,10 +1,56 @@
 import { useEffect } from "react"
+import { useForm } from "react-hook-form"
+import FormActions from "~/components/app/form-actions"
+import FormGroup from "~/components/app/form-group"
+import FormMessage from "~/components/app/form-message"
+import { useFormResult } from "~/lib/context/form-result-context"
 import { useSubTitle } from "~/lib/context/sub-title-context"
 
 export default function SelectMany() {
     const {setTitle} = useSubTitle()
     useEffect(() => setTitle("Select Many Form Items"), [setTitle])
+
+    const {setResult} = useFormResult()
+    useEffect(setResult, [])
+
+    const {handleSubmit, reset, register, formState : {errors}} = useForm<SelectManyForm>()
+
+    const saveAction = (form : SelectManyForm) => setResult(JSON.stringify(form, null, 2))
+    const clear = () => {
+        reset()
+        setResult()
+    }
+
     return (
-        <></>
+        <form onSubmit={handleSubmit(saveAction)}>
+
+            <FormGroup label="Select Options" className="mb-3">
+                <select {...register('selects', {validate : (array) => array.length > 0})} multiple className="border-1 rounded-md p-2 w-[200px]">
+                {Array.from({length : 3}).map((_, index) => 
+                    <option key={index} value={`Level ${index + 1}`}>{`Level ${index + 1}`}</option>
+                )}
+                </select>
+                {errors.selects && <FormMessage message="Option Selects is required." />}
+            </FormGroup>
+
+            <FormGroup label="Select Options" className="mb-3">
+                <div>
+                    {Array.from({length : 3}).map((_, index) => 
+                        <label key={index} htmlFor={`check-${index}`} className="me-3">
+                            <input {...register('checks', {validate : (checks) => checks.length > 0})} id={`check-${index}`} type="checkbox" value={index + 1} className="me-2" />
+                            <span>{`Value ${index + 1}`}</span>
+                        </label>
+                    )}
+                </div>
+                {errors.checks && <FormMessage message="Check value is required." />}
+            </FormGroup>
+
+            <FormActions clear={clear} />
+        </form>
     )
+}
+
+type SelectManyForm = {
+    checks : number []
+    selects : string []
 }
--- using-forms/app/routes/basic/select/select-one.tsx
+++ using-forms/app/routes/basic/select/select-one.tsx
@@ -1,21 +1,55 @@
 import { useEffect } from "react"
 import { useForm } from "react-hook-form"
+import FormActions from "~/components/app/form-actions"
+import FormGroup from "~/components/app/form-group"
+import FormMessage from "~/components/app/form-message"
+import { Label } from "~/components/ui/label"
 import { useFormResult } from "~/lib/context/form-result-context"
 import { useSubTitle } from "~/lib/context/sub-title-context"
 
 export default function SelectOne() {
     const {setTitle} = useSubTitle()
-    useEffect(() => setTitle("Select One Form Items"), [setTitle])
-
-    const {handleSubmit} = useForm<SelectOneForm>()
     const {setResult} = useFormResult()
+
+    useEffect(() => setTitle("Select One Form Items"), [setTitle])
+    useEffect(setResult, [])
+
+    const {handleSubmit, reset, register, formState : {errors}} = useForm<SelectOneForm>()
 
     const submitAction = (form: SelectOneForm) => {
         setResult(JSON.stringify(form, null, 2))
     }
 
+    const clear = () => {
+        reset()
+        setResult()
+    }
+
     return (
-        <form onSubmit={handleSubmit(submitAction)}></form>
+        <form onSubmit={handleSubmit(submitAction)}>
+
+            <FormGroup label="Radio Select" className="mb-3">
+                {Array.from({length : 3}).map((_, index) => 
+                    <Label htmlFor={`radio-${index}`} className="mb-2">
+                        <input {...register('radio', {required : true})} type="radio" id={`radio-${index}`} value={index} />
+                        <span>{`Level ${index + 1}`}</span>
+                    </Label>
+                )}
+                {errors.radio && <FormMessage message="Radio Select Value is required ." />}
+            </FormGroup>
+
+            <FormGroup label="Option Select" className="mb-3">
+                <select {...register('select', {required : true})} className="w-[200px] border-2 p-1.5 rounded-lg text-sm">
+                    <option value="">Select One</option>
+                    {Array.from({length : 3}).map((_, index) => 
+                        <option value={index}>{`Option ${index + 1}`}</option>
+                    )}
+                </select>
+                {errors.select && <FormMessage message="Option Select Value is required ." />}
+            </FormGroup>
+
+            <FormActions clear={clear} />
+        </form>
     )
 }
 
